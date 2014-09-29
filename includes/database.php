2d8c87288352277ee287e008166dd3e4bfa04fc8
Sabbir Rahman Sakib <sakib@Sabbirs-MacBook-Pro.local>
2014-09-29 11:45:45 +0200

    database.php file is ready

--- includes/database.php
+++ includes/database.php
@@ -33,6 +33,32 @@ class MYSQLDatabase{
             unset($this->connection);
         }
     }
+    public function query($sql){
+        // 3. Perform database query
+
+        $result = mysql_query($sql, $this->connection);
+        $this->confirm_query($result);
+        return $result;
+    }
+    public function mysql_prep( $value ) {
+        $magic_quotes_active = get_magic_quotes_gpc();
+        $new_enough_php = function_exists( "mysql_real_escape_string" ); // i.e. PHP >= v4.3.0
+        if( $new_enough_php ) { // PHP v4.3.0 or higher
+            // undo any magic quote effects so mysql_real_escape_string can do the work
+            if( $magic_quotes_active ) { $value = stripslashes( $value ); }
+            $value = mysql_real_escape_string( $value );
+        } else { // before PHP v4.3.0
+            // if magic quotes aren't already on then add slashes manually
+            if( !$magic_quotes_active ) { $value = addslashes( $value ); }
+            // if magic quotes are active, then the slashes already exist
+        }
+        return $value;
+    }
+    private function confirm_query($result){
+        if (!$result) {
+            die("Database query failed: " . mysql_error());
+        }
+    }
 }
 
 $database = new MYSQLDatabase();
