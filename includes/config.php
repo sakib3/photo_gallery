12466040920a63a3ba4a5fb48297eecd5e552a96
Sabbir Rahman Sakib <sakib@Sabbirs-MacBook-Pro.local>
2014-09-29 11:03:49 +0200

    mysql db structure added and config file for connection is added

---
+++ includes/config.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sakib
+ * Date: 29/09/14
+ * Time: 10:55
+ */
+//Database Constants
+defined("DB_SERVER")? null : define("DB_SERVER", "localhost");
+defined("DB_USER")? null : define("DB_USER", "gallery");
+defined("DB_PASS")? null : define("DB_PASS","123456");
+defined("DB_NAME")? null : define("DB_NAME", "photo_gallery");
+
+?>
